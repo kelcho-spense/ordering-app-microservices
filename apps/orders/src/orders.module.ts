a9c5977030cff03431e52603b7ed74852efa3997
KevinComba <kelchospense88@gmail.com>
2025-02-01 21:14:21 +0300

    add configuration for MongoDB and update environment handling in orders module

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
