10003fda5583f698161978b9fc5102ef1776720f
KevinComba <kelchospense88@gmail.com>
2025-02-02 21:11:01 +0300

    feat: implement createOrder endpoint and update orders module Dockerfile

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { createOrderRequest } from './dto/create-order.request';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
   async createOrder(@Body() request: createOrderRequest) {
-    return await this.ordersService.createOrder(request);
+    return this.ordersService.createOrder(request);
   }
 }
--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { createOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly orderRepository: OrdersRepository) {}
+
+  async createOrder(request: createOrderRequest) {
+    return this.orderRepository.create(request);
   }
+
+  // async getOrders() {
+  //   return this.orderRepository.find();
+  // }
+
+  // async getOrderById(id: string) {
+  //   return this.orderRepository.findOne(id);
+  // }
+
+  // async updateOrder(id: string, request: createOrderRequest) {
+  //   return this.orderRepository.findOneAndUpdate(id, request);
+  // }
 }
