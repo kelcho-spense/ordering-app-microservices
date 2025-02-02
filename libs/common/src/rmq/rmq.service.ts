80b813d0797e40915b8cb7e47aebcd8a395217aa
KevinComba <kelchospense88@gmail.com>
2025-02-02 22:26:12 +0300

    feat: add RMQ module and service for RabbitMQ integration

---
+++ libs/common/src/rmq/rmq.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { RmqOptions, Transport } from '@nestjs/microservices';
+
+@Injectable()
+export class RmqService {
+  constructor(private readonly configService: ConfigService) {}
+  getOptions(queue: string, noAck: boolean = false): RmqOptions {
+    return {
+      transport: Transport.RMQ, // This is to specify that we are using RabbitMQ
+      options: {
+        urls: [this.configService.get<string>('RABBIT_MQ_URL')!],
+        queue: this.configService.get<string>(`RABBIT_MQ_${queue}_QUEUE`), // This is to get the queue name from the environment variables
+        noAck, // This is to specify if the message should be acknowledged
+        persistent: true, // This is to ensure that the message is not lost
+      },
+    };
+  }
+}
